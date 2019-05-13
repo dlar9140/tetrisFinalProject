6f34eba60b242530e904149efeac773a87bcc8f5
David <davidlarson46@yahoo.com>
2019-05-13 09:51:42 -0600

    loops to add in a new piece after one is used.

--- src/gamespace/TetrisProject.java
+++ src/gamespace/TetrisProject.java
@@ -3,6 +3,7 @@ package gamespace;
 import java.awt.Color;
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TetrisProject
 {
@@ -45,6 +46,37 @@ public class TetrisProject
 	private ArrayList<Integer> nextPiece = new ArrayList<Integer>();
 	private long score;
 	private Color [][] well;
+	
+	private void init()
+	{
+		well = new Color[12][24];
+		for(int i=0; i<12; i++)
+		{
+			for(int j=0; j<23; j++)
+			{
+				if(i==0||i==11||i==22)
+				{
+					well[i][j]=Color.PINK;
+				}
+				else
+				{
+					well[i][j]=Color.BLACK;
+				}
+			}
+		}
+		newPiece();
+	}
+	
+	public void newPiece()
+	{
+		pt=new Point (5,2);
+		rotation = 0;
+		if(nextPiece.isEmpty())
+		{
+			Collections.addAll(nextPiece, 0, 1, 2, 3);
+			Collections.shuffle(nextPiece);
+		}
+	}
 
 	
 	
