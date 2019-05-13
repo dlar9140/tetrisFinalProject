c5ac06e954086d4945e681ac1b568faba22bdcdc
David <davidlarson46@yahoo.com>
2019-05-13 08:30:28 -0600

    Made some points to get started.

--- src/gamespace/TetrisProject.java
+++ src/gamespace/TetrisProject.java
@@ -1,6 +1,25 @@
 package gamespace;
 
+import java.awt.Point;
+
 public class TetrisProject
 {
+	private final Point [][][] myPoint = {
+			{
+				{new Point(0,1),new Point(1,1),new Point(2,1),new Point(3,1)},
+				{new Point(1,0),new Point(1,1),new Point(1,2),new Point(1,3)},
+				{new Point(0,1),new Point(1,1),new Point(2,1),new Point(3,1)},
+				{new Point(1,0),new Point(1,1),new Point(1,2),new Point(1,3)}
+			}
+	
+	};
+
+
+public static void main(String [] args)
+{
+	
+}
 
 }
+	
+
--- src/module-info.java
+++ src/module-info.java
@@ -7,4 +7,5 @@
  */
 module tetrisFinalProject
 {
+	requires java.desktop;
 }
